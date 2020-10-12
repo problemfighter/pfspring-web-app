930e4d3141005587339c95eec41f0dcf815dbdf1
Touhid Mia <hmtm.cse@gmail.com>
2020-10-12 13:24:53 +0600

    Redis

--- application/web-app/src/main/java/com/problemfighter/pfspring/webapp/WebApp.java
+++ application/web-app/src/main/java/com/problemfighter/pfspring/webapp/WebApp.java
@@ -3,8 +3,10 @@ package com.problemfighter.pfspring.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.*"})
+@EnableCaching
 public class WebApp {
 
 	public static void main(String[] args) {
