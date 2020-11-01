700660aef706e2fedb2ed1f79b4e99f14d940355
Touhid Mia <hmtm.cse@gmail.com>
2020-11-01 11:45:18 +0600

    config

--- application/web-app/src/main/java/com/problemfighter/pfspring/webapp/WebApp.java
+++ application/web-app/src/main/java/com/problemfighter/pfspring/webapp/WebApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.*"})
-@EnableCaching
 public class WebApp {
 
 	public static void main(String[] args) {
